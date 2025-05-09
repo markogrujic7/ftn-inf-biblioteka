3f4de650e258d766d29b4e509bec458c56fc0066
Nikola Davidovac <davidovacnikola@gmail.com>
2025-05-09 03:54:40 +0200

    Dodate sve funcionalnosti za A2 i B2

--- library/app/bookview/bookview.js
+++ library/app/bookview/bookview.js
@@ -10,34 +10,39 @@ class Knjiga {
         this.tip = popularnost;
     }
 }
+let knjige = []
 
 function ucitajKnjige() {
-    const sacuvaneKnjige = localStorage.getItem('knjige');
-    if (sacuvaneKnjige) {
-        const parsedKnjige = JSON.parse(sacuvaneKnjige);
-        return parsedKnjige.map(knjiga => new Knjiga(knjiga.id, knjiga.naziv, knjiga.datum, knjiga.url, knjiga.opis, knjiga.popularnost));
+    const sacuvaneKnjige = localStorage.getItem('knjige')
+    const parsirane = JSON.parse(sacuvaneKnjige)
+    if (parsirane && parsirane.length > 0) {
+        knjige = JSON.parse(sacuvaneKnjige)
+        prikaziTabelu()
     }
-    return [
-        new Knjiga(
+    else{
+        let knjiga
+        knjiga = new Knjiga(
             1,
             "Rat i mir",
             "1869",
             "",
             "Klasični ruski roman",
             5
-        ),
-        new Knjiga(
+        )
+        knjige.push(knjiga)
+        knjiga = new Knjiga(
             2,
             "Ana Karenjina",
             "1877",
             "",
             "Drama ruske aristokratije",
             4
-        ),
-    ];
+        )
+        knjige.push(knjiga)
+        prikaziTabelu()
+    }
+    dodajKnjigu()
 }
-
-let knjige = ucitajKnjige();
 
 const tbody = document.getElementById('tabela-body');
 
@@ -74,4 +79,40 @@ function izbrisiKnjigu(id){
     prikaziTabelu();
 }
 
-prikaziTabelu();
+function dodajKnjigu() {
+    let submitBtn = document.querySelector('#submitBtn')
+    submitBtn.addEventListener('click', function() {
+        const forma = document.querySelector('#forma')
+        const formData = new FormData(forma)
+
+        const naziv = formData.get('naziv')
+        const godina = formData.get('godina')
+        const slika = formData.get('slikaURL')
+        const opis = formData.get('opis')
+        const popularnost = formData.get('popularnost')
+        if (!naziv || naziv === "" || parseInt(godina) < 1 || !opis || opis === "" || parseInt(popularnost) > 5 || parseInt(popularnost) < 1){
+            alert("Unesi validne podatke!")
+            return
+        }
+        const id = generisiId(knjige)
+
+        let knjiga = new Knjiga(id, naziv, godina, slika, opis, popularnost)
+        knjige.push(knjiga)
+        localStorage.setItem('knjige', JSON.stringify(knjige))
+
+        prikaziTabelu()
+    })
+}
+
+function generisiId(knjige) {
+    let id = 0
+    for (const knjiga of knjige) {
+        if (id < knjiga.id){
+            id = knjiga.id
+        }
+    }
+    
+    return id + 1
+}
+
+document.addEventListener('DOMContentLoaded', ucitajKnjige)
--- library/app/mybooks/mybooks.js
+++ library/app/mybooks/mybooks.js
@@ -12,27 +12,51 @@ class Knjiga {
 }
 
 function ucitajKnjige(){
-    const sacuvaneKnjige = localStorage.getItem('knjige');
-    if (sacuvaneKnjige) {
-        const parsedKnjige = JSON.parse(sacuvaneKnjige);
-        return parsedKnjige.map(knjiga => new Knjiga(knjiga.id, knjiga.naziv, knjiga.datum, knjiga.url, knjiga.opis, knjiga.popularnost));
-    }
-    return [];
+    if (localStorage.getItem('knjige')) {
+    if (JSON.parse(localStorage.getItem('knjige')).length > 0 && localStorage.getItem('knjige')){
+        const sacuvaneKnjige = localStorage.getItem('knjige');
+        const parsedSaved = JSON.parse(sacuvaneKnjige)
+        sveKnjige = parsedSaved
+
+        if (localStorage.getItem('iznajmljeneKnjige')){
+        if (JSON.parse(localStorage.getItem('iznajmljeneKnjige')).length > 0){
+            const iznajmljene = localStorage.getItem('iznajmljeneKnjige')
+            const parsedTaken = JSON.parse(iznajmljene)
+            iznajmljeneKnjige = parsedTaken
+            prikaziIznajmljeneKnjige()
+        }}
+
+        pronadjiSlobodne()
+        prikaziSlobodneKnjige()
+
+    }}
 }
 
-function ucitajIznajmljeneKnjige() {
-    const iznajmljeneKnjige = localStorage.getItem('iznajmljeneKnjige');
+let sveKnjige = []
+let iznajmljeneKnjige = []
+let slobodneKnjige = []
 
-    if (iznajmljeneKnjige) {
-        const parsedIznajmljeneKnjige = JSON.parse(iznajmljeneKnjige);
-        return parsedIznajmljeneKnjige.map(knjiga => new Knjiga(knjiga.id, knjiga.naziv, knjiga.datum, knjiga.url, knjiga.opis, knjiga.popularnost));
+function pronadjiSlobodne() {
+    for (const knjiga of sveKnjige) {
+        let isIt = true
+        if (iznajmljeneKnjige.length < 1 || !iznajmljeneKnjige){
+            slobodneKnjige = sveKnjige
+            localStorage.setItem('slobodneKnjige', JSON.stringify(sveKnjige))
+            return
+        }
+        for (const iznajmljena of iznajmljeneKnjige) {
+            if (knjiga.id == iznajmljena.id){
+                isIt = false
+                break
+            }
+        }
+        if (isIt) {
+            slobodneKnjige.push(knjiga)
+            localStorage.setItem('slobodneKnjige', JSON.stringify(slobodneKnjige))
+        }
     }
-    return [];
 }
-
-let sveKnjige = ucitajKnjige();
-let iznajmljeneKnjige = ucitajIznajmljeneKnjige();
-const tbody = document.getElementById('tabela-body');
+const tbody = document.getElementById('tabela-body')
 
 function sacuvajIznajmljeneKnjige() {
     localStorage.setItem('iznajmljeneKnjige', JSON.stringify(iznajmljeneKnjige));
@@ -65,6 +89,48 @@ function vratiKnjigu(id){
     iznajmljeneKnjige = iznajmljeneKnjige.filter(knjiga => knjiga.id !== id);
     sacuvajIznajmljeneKnjige();
     prikaziIznajmljeneKnjige();
+    prikaziSlobodneKnjige()
 }
 
-prikaziIznajmljeneKnjige();
+function prikaziSlobodneKnjige() {
+    let tBody = document.querySelector("#tBody")
+    tBody.innerHTML = ''
+
+    for (const knjiga of slobodneKnjige) {
+        const tr = document.createElement("tr")
+
+        const br = document.createElement("td")
+        br.textContent = knjiga.id
+        const naziv = document.createElement("td")
+        naziv.textContent = knjiga.naziv
+        const buttonId = document.createElement("td")
+        const button = document.createElement("button")
+        button.textContent = "Iznajmi"
+        buttonId.appendChild(button)
+        tr.appendChild(br)
+        tr.appendChild(naziv)
+        tr.appendChild(buttonId)
+        tBody.appendChild(tr)
+
+        button.addEventListener('click', function () {
+            iznajmiKnjigu(knjiga)
+        })
+    }
+}
+
+function iznajmiKnjigu(knjiga) {
+    let iznajmljena = null
+    for (const element of slobodneKnjige) {
+        if (element.id == knjiga.id){
+            iznajmljena = element
+        }
+    }
+    slobodneKnjige.splice(slobodneKnjige.indexOf(iznajmljena), 1)
+    iznajmljeneKnjige.push(iznajmljena)
+    localStorage.setItem('slobodneKnjige', JSON.stringify(slobodneKnjige))
+    localStorage.setItem('iznajmljeneKnjige', JSON.stringify(iznajmljeneKnjige))
+    prikaziSlobodneKnjige()
+    prikaziIznajmljeneKnjige()
+}
+
+document.addEventListener('DOMContentLoaded', ucitajKnjige)
